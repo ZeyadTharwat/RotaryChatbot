c0bd0053102ffafe0757f1090eab5028713cf0d0
ZeyadTharwat <zeyadtharwatcf@gmail.com>
2024-06-26 04:10:35 +0300

    first commit

---
+++ src/Widget/WidgetLayout/Keypad/index.jsx
@@ -0,0 +1,97 @@
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import { useContext, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import { createUserMessage } from "../../../utils/helpers";
+import AppContext from "../../AppContext";
+import {
+  addMessage,
+  fetchBotResponse,
+  toggleBotTyping,
+  toggleUserTyping,
+} from "../Messages/messageSlice";
+import axios from "axios";
+
+const Textarea = styled.textarea`
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const Keypad = () => {
+  const dispatch = useDispatch();
+  const theme = useContext(AppContext);
+  const [userInput, setUserInput] = useState("");
+  const userTypingPlaceholder = useSelector(
+    (state) => state.messageState.userTypingPlaceholder
+  );
+
+  const userTyping = useSelector((state) => state.messageState.userTyping);
+  const { rasaServerUrl, userId, textColor } = theme;
+
+  const handleSubmit = async () => {
+    if (userInput.length > 0) {
+      // Send user message to API endpoint
+      try {
+        const response = await axios.post("https://test-api-url/ask_rotary", {
+          message: userInput.trim(),
+        });
+        console.log("Message sent successfully:", response.data);
+      } catch (error) {
+        console.error("Error sending message:", error);
+        // Handle error as needed
+      }
+
+      // Dispatch actions for messaging system
+      dispatch(addMessage(createUserMessage(userInput.trim())));
+      setUserInput("");
+      dispatch(toggleUserTyping(false));
+      dispatch(toggleBotTyping(true));
+      dispatch(
+        fetchBotResponse({
+          rasaServerUrl,
+          message: userInput.trim(),
+          sender: userId,
+        })
+      );
+    }
+  };
+
+  return (
+    <div className="mt-auto flex h-[12%] items-center rounded-t-3xl rounded-b-[2rem] bg-slate-50">
+      <Textarea
+        rows="1"
+        className={`mx-4 block w-full resize-none bg-slate-50 p-2.5 text-sm text-gray-900 outline-none ${
+          userTyping ? "cursor-default" : "cursor-not-allowed"
+        }`}
+        placeholder={userTypingPlaceholder}
+        value={userInput}
+        onChange={(e) => {
+          setUserInput(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            handleSubmit();
+          }
+        }}
+        readOnly={!userTyping}
+      />
+      <button
+        type="submit"
+        className={`${
+          userInput.trim().length > 1 ? "cursor-default" : "cursor-not-allowed"
+        } inline-flex justify-center rounded-full p-2 hover:bg-slate-100`}
+        style={{ color: textColor }}
+        onClick={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <PaperAirplaneIcon className="h-6 w-6 -rotate-45 stroke-[1.1px]" />
+      </button>
+    </div>
+  );
+};
